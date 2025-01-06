4a43a6de50ef5cbe214c97acdc9bda646fa1c193
jeswin-p <jeswin23231@gmail.com>
2025-01-06 15:46:36 +0530

    student listt

--- Router.js
+++ Router.js
@@ -5,6 +5,7 @@ const staff_controller=require("./collection/staff_collection")
 
 route.post("/savestudent",controller.upload,controller.savestudent)
 route.post("/loginstudent",controller.LoginStudent)
+route.get("/studentlist",controller.StudentList)
 
 
 route.post("/savestaff",staff_controller.upload,staff_controller.savestaff)
